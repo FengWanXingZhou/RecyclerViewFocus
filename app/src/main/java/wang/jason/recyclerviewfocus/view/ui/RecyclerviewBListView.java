4c82db15f95a16d6caaaa3dcce1d5c805143abb4
FengWanXingZhou <992782801@qq.com>
2018-07-31 16:39:59 +0800

    commit

--- app/src/main/java/wang/jason/recyclerviewfocus/view/ui/RecyclerviewBListView.java
+++ app/src/main/java/wang/jason/recyclerviewfocus/view/ui/RecyclerviewBListView.java
@@ -63,16 +63,20 @@ public class RecyclerviewBListView extends RecyclerView{
         int curFocusChildIndex = getFocusedChildIndex();
 
         if(curFocusChildIndex == -1){
+
             return null;
         }
 
         if(direction == View.FOCUS_UP || direction == View.FOCUS_DOWN){
 
-
-
             int nextChildIndex = direction == View.FOCUS_UP?curFocusChildIndex-1:curFocusChildIndex+1;
 
             int itemHeight = getResources().getDimensionPixelOffset(R.dimen.item_height);
+
+            /**
+             * 如果这里为true，且此时数据正在刷新，那么很大几率焦点会异常，因此我们手动处理，我们根据方向手动滚动一个子Item的高度，
+             * 这时候再重新调用focusSearch时，curFocusChildIndex会变化，下一次执行这里就为false了。
+             */
 
             if(nextChildIndex < 0 || nextChildIndex >= getChildCount()){
 
@@ -85,12 +89,23 @@ public class RecyclerviewBListView extends RecyclerView{
                 return focusSearch(focused,direction);
 
             }
+            /**
+             * 获取下一个子Item
+             */
 
             View nextChild = getChildAt(nextChildIndex);
+
+            /**
+             * 用于保存下一个子Item可以被焦点的View
+             */
             ArrayList<View> focusables = new ArrayList<>();
 
             if(nextChild != null) {
+                /**
+                 * 找到可以被焦点的View
+                 */
                 findFocusables(nextChild, focusables);
+
                 View targetView = null;
                 for (View focusableView : focusables) {
                     Log.i("focusSearch","focous focusableView ");
@@ -100,6 +115,9 @@ public class RecyclerviewBListView extends RecyclerView{
                         break;
                     }
                 }
+                /**
+                 * return我们想要焦点的选项，
+                 */
                 if (targetView != null) {
                     Log.i("focusSearch","focous focusableView ,return targetView");
                     return targetView;
@@ -116,14 +134,26 @@ public class RecyclerviewBListView extends RecyclerView{
         return super.focusSearch(focused, direction);
     }
 
+    /**
+     * 找到当前被焦点的子item的index，注意这个index不是指dapter中的position，而是指当前可见区域内的位置
+     * @return
+     */
     private int getFocusedChildIndex() {
+
         for (int i = 0; i < getChildCount(); ++i) {
+
             if (getChildAt(i).hasFocus()) {
                 return i;
             }
         }
         return -1;
     }
+
+    /**
+     * 找到可以被焦点的子item，如果子item是ViewGroup，则递归查找,
+     * @param v
+     * @param outFocusable
+     */
     private static void findFocusables(View v, ArrayList<View> outFocusable) {
         if (v.isFocusable()) {
             outFocusable.add(v);
